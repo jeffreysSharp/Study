5849ef7f951fca3ae6866a4dff617067f0f347d9
Jeferson Almeida <jeferson.almeida@nava.com.br>
2024-07-16 12:49:19 -0300

    Iniciando com ASPTNETCore

---
+++ FormacaoFullStack/IniciandoASPNETCore/DemoAPI/DemoAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,47 @@
+using DemoAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("alunos")]
+        public Aluno GetAlunos()
+        {
+            return new Aluno()
+            {
+                Nome = "Eduardo",
+                Ativo = true,
+                DataCadastro = DateTime.Now,
+                Email = "teste@teste.com",
+                Id = 1
+            };
+        }
+    }
+}
---
+++ FormacaoFullStack/IniciandoASPNETCore/DemoBlazorAsm/DemoBlazorAsm/Server/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace DemoBlazorAsm.Server.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
