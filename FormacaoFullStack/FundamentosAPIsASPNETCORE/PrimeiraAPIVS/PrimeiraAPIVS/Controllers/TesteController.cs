c435b4313406b58bb7a79db810b80a3816df5a54
Jeferson Almeida <jeferson.almeida@nava.com.br>
2024-07-17 18:23:23 -0300

    Formatadores de resposta

--- FormacaoFullStack/FundamentosAPIsASPNETCORE/PrimeiraAPIVS/PrimeiraAPIVS/Controllers/TesteController.cs
+++ FormacaoFullStack/FundamentosAPIsASPNETCORE/PrimeiraAPIVS/PrimeiraAPIVS/Controllers/TesteController.cs
@@ -7,23 +7,30 @@ namespace PrimeiraAPIVS.Controllers
     public class TesteController : ControllerBase
     {
         [HttpGet]
+        [ProducesResponseType(typeof(Produto), StatusCodes.Status200OK)]
         public IActionResult Get()
         {
             return Ok(new Produto { Id = 1, Nome = "Jeferson Especialista em .NET" });
         }
 
         [HttpGet("{id:int}")]
+        [ProducesResponseType(typeof(Produto), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(Produto), StatusCodes.Status404NotFound)]
         public IActionResult Get(int id)
         {
             return Ok(new Produto { Id = 1, Nome = "Teste" });
         }
-        [HttpPost]
+
+        [ProducesResponseType(typeof(Produto), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public IActionResult Post(Produto produto)
         {
             return CreatedAtAction("Get", new { id = produto.Id }, produto);
         }
 
         [HttpPut("id")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public IActionResult Put(int id, Produto produto)
         {
             if (id != produto.Id) return BadRequest();
@@ -31,6 +38,7 @@ namespace PrimeiraAPIVS.Controllers
         }
 
         [HttpDelete("id:int")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
         public IActionResult Delete(int id)
         { 
             return NoContent(); 
