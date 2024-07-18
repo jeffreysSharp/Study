bcb150da22fcbc71944ea0f262f50a4d648f5583
Jeferson Almeida <jeferson.almeida@nava.com.br>
2024-07-18 14:16:20 -0300

    Suporte de JWT no Swagger

--- FormacaoFullStack/FundamentosAPIsASPNETCORE/ApiFuncional/ApiFuncional/Program.cs
+++ FormacaoFullStack/FundamentosAPIsASPNETCORE/ApiFuncional/ApiFuncional/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,7 +19,35 @@ builder.Services.AddControllers()
    });
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen( c =>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "Insira o Token desta maneira: Bearer {seu token}",
+        Name =  "Autorizathion", 
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}        
+        }
+    });
+
+});
 
 builder.Services.AddDbContext<ApiDbContext>(options =>
 {
