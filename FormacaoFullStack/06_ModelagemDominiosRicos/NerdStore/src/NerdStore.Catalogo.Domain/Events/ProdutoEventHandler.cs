4126f4788733e3d4ff62b27121b7244261d93f2f
Jeferson Almeida <jeferson.almeida@nava.com.br>
2024-07-26 09:11:12 -0300

    Lançando eventos de domínio

---
+++ FormacaoFullStack/06_ModelagemDominiosRicos/NerdStore/src/NerdStore.Catalogo.Domain/Events/ProdutoEventHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catalogo.Domain.Events
+{
+    public class ProdutoEventHandler : INotificationHandler<ProdutoAbaixoEstoqueEvent>
+    {
+        private readonly IProdutoRepository _produtoRepository;
+
+        public ProdutoEventHandler(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        public async Task Handle(ProdutoAbaixoEstoqueEvent mensagem, CancellationToken cancellationToken)
+        {
+            var produto = await _produtoRepository.ObterPorId(mensagem.AggregateId);
+
+            // Enviar e-mail para aquisição de mais produto
+
+        } 
+    }
+}
---
+++ FormacaoFullStack/06_ModelagemDominiosRicos/NerdStore/src/NerdStore.Core/Messages/Event.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace NerdStore.Core.Messages
+{
+    public abstract class Event : Message, INotification
+    {
+        public DateTime TimeStamp { get; set; }
+
+        public Event()
+        {
+            TimeStamp = DateTime.Now;   
+        }
+    }
+}
