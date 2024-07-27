5565eae2d4475167891c274bcc6de56e73e4e8fc
jeffreysSharp <jefferson_qi3@hotmail.com>
2024-07-27 18:44:45 -0300

    Organização

--- FormacaoFullStack/back-end/Java/JavaCompleto/exercicios/src/fundamentos/PrimeiroPrograma.java
+++ FormacaoFullStack/back-end/Java/JavaCompleto/exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -4,6 +4,6 @@ public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
 		System.out.println("Primeiro Programa Parte #1");
-		System.out.println("Primeiro Programa Parte #1");
+		System.out.println("Primeiro Programa Parte #2");
 	}
 }
