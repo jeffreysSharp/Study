3d0897fd4753f65e9850c5a8f76e799c6c891754
jeffreysSharp <jefferson_qi3@hotmail.com>
2024-07-28 08:35:33 -0300

    Inferencia de tipos com erro no var

--- FormacaoFullStack/back-end/Java/JavaCompleto/exercicios/src/fundamentos/Inferencia.java
+++ FormacaoFullStack/back-end/Java/JavaCompleto/exercicios/src/fundamentos/Inferencia.java
@@ -3,12 +3,11 @@ package fundamentos;
 public class Inferencia {
 
 	public static void main(String[] args) {
-
 		double a = 4.5; // variável inicializada
 		System.out.println(a);
 		
 		a= 12;
-		System.out.println(0);
+		System.out.println(a);
 		
 		// a= "..."; //erro 
 		
@@ -21,13 +20,13 @@ public class Inferencia {
 		
 		c = "Outro Texto";
 		System.out.println(c);
-		*/
+		*/	
 		
 		// c = 4.5;
 		
 		double d; // variável foi declarada
 		d = 123.65; //variável foi inicializada
-		System.out.println(d); // variável foi utilizada	
+		System.out.println(d); // variável foi utilizada
 		
 	}
 
