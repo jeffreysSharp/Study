67864e81a5654af3604e72855a737b54a5c8023f
jeffreysSharp <jefferson_qi3@hotmail.com>
2024-07-28 05:01:29 -0300

    Váriaveis e constantes

---
+++ FormacaoFullStack/back-end/Java/JavaCompleto/exercicios/src/fundamentos/AreaCircunferencia.java
@@ -0,0 +1,27 @@
+package fundamentos;
+
+public class AreaCircunferencia {
+	public static void main(String[] args) {
+		
+		double raio = 3.4;
+		
+		// Constante 
+		final double PI = 3.14159;
+		
+		
+		// Fórmula para calcular circunferência
+		double area = PI * raio * raio;
+		System.out.println(area);
+		
+		raio = 10;
+		area = PI * raio * raio;
+		
+		// Resultado da área da Circunferência
+		// System.out.println(pi * raio * raio);
+		System.out.println("Área = " + area + "m2.");
+		
+		
+	
+	}
+
+}
