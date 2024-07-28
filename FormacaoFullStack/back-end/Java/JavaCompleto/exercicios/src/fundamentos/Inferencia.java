a15d6891d78d76226f2f0e2b408102e68a938048
jeffreysSharp <jefferson_qi3@hotmail.com>
2024-07-28 08:24:02 -0300

    Inferência de Tipos

---
+++ FormacaoFullStack/back-end/Java/JavaCompleto/exercicios/src/fundamentos/Inferencia.java
@@ -0,0 +1,34 @@
+package fundamentos;
+
+public class Inferencia {
+
+	public static void main(String[] args) {
+
+		double a = 4.5; // variável inicializada
+		System.out.println(a);
+		
+		a= 12;
+		System.out.println(0);
+		
+		// a= "..."; //erro 
+		
+		/* Verificar por que não reconhece var 
+		var b = 4.5;
+		System.out.println(b);
+		
+		var c = "Texto";
+		System.out.println(c);		
+		
+		c = "Outro Texto";
+		System.out.println(c);
+		*/
+		
+		// c = 4.5;
+		
+		double d; // variável foi declarada
+		d = 123.65; //variável foi inicializada
+		System.out.println(d); // variável foi utilizada	
+		
+	}
+
+}
