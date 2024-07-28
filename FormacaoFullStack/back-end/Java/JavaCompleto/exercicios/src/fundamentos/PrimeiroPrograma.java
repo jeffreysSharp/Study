f99911080ec9e6c3bedfca45d86c789542d2d988
jeffreysSharp <jefferson_qi3@hotmail.com>
2024-07-28 04:35:25 -0300

    Documentação

--- FormacaoFullStack/back-end/Java/JavaCompleto/exercicios/src/fundamentos/PrimeiroPrograma.java
+++ FormacaoFullStack/back-end/Java/JavaCompleto/exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,9 +1,29 @@
 package fundamentos;
 
+/**
+ * Essa classe representa JavaDoc
+ * 
+ * @author Jeferson Almeida
+ * Jeferson Almeida 
+ * jefferson_qi3@hotmasil.com
+ * since JDK 13
+ */
 public class PrimeiroPrograma {
-
+	/**
+	 * JavaDoc
+	 * @param args
+	 */
 	public static void main(String[] args) {
+		// Uma sentença de código termina com ;
+		// Mais um comentário...
+		// Fim
 		System.out.println("Primeiro Programa Parte #1");
+		
+		/*
+		 Linha 1
+		 Linha 2
+		 */	
+		
 		System.out.println("Primeiro Programa Parte #2");
 	}
 }
