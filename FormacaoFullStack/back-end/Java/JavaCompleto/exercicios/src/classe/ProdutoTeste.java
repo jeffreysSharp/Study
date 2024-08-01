ca28cd12d5abbad39157f09de7567866bbacd0d7
Jeferson Almeida <jeferson.almeida@nava.com.br>
2024-08-01 10:32:34 -0300

    Construtor #02

--- FormacaoFullStack/back-end/Java/JavaCompleto/exercicios/src/classe/ProdutoTeste.java
+++ FormacaoFullStack/back-end/Java/JavaCompleto/exercicios/src/classe/ProdutoTeste.java
@@ -4,13 +4,9 @@ public class ProdutoTeste {
 
 	public static void main(String[] args) {
 	
-		Produto produto1 = new Produto();
-		produto1.nome = "Notebook";
-		produto1.preco = 4356.89;
-		produto1.desconto = 0.25;
+		Produto produto1 = new Produto("Notebook", 4356.89, 0.25);		
 		
-		
-		Produto produto2 = new Produto();
+		Produto produto2 = new Produto("Caneta Preta");
 		produto2.nome = "Caneta Preta";
 		produto2.preco = 12.56;
 		produto2.desconto = 0.29;
