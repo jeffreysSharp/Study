cc46e3e8c81adfe6a20bd0edd7b748258761d735
Jeferson Almeida <jeferson.almeida@nava.com.br>
2024-08-01 17:52:14 -0300

    This e This()

--- FormacaoFullStack/back-end/Java/JavaCompleto/exercicios/src/classe/Data.java
+++ FormacaoFullStack/back-end/Java/JavaCompleto/exercicios/src/classe/Data.java
@@ -7,19 +7,21 @@ public class Data {
 	int _ano;
 	
 	Data() {
-		_dia = 1;
-		_mes = 1;
-		_ano = 1970;
+		// _dia = 1;
+		// _mes = 1;
+		// _ano = 1970;
+		this(1, 1, 1970);
+		
 	}
 	
-	Data(int dia, int mes, int ano) {
-		_dia = dia;
-		_mes = mes;
-		_ano = ano;
+	Data(int _dia, int _mes, int _ano) {
+		this._dia = _dia;
+		this._mes = _mes;
+		this._ano = _ano;
 	}
 	
 	String obterDataFormatada() {		
-		return String.format("%d/%d/%d", _dia,  _mes, _ano); 
+		return String.format("%d/%d/%d", this._dia,  _mes, _ano); 
 	}
 	
 	void imprimirDataFormatada() {
