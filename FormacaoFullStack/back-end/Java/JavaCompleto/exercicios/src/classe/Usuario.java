b17c51036bbead7eb83d0ba7ef00e3290a85d2a1
Jeferson Almeida <jeferson.almeida@nava.com.br>
2024-08-03 12:21:13 -0300

    Recursos

--- FormacaoFullStack/back-end/Java/JavaCompleto/exercicios/src/classe/Usuario.java
+++ FormacaoFullStack/back-end/Java/JavaCompleto/exercicios/src/classe/Usuario.java
@@ -3,23 +3,5 @@ package classe;
 public class Usuario {
 	String nome;
 	String email;
-	
-	@Override
-	public boolean equals(Object objeto) {
-		
-		if(objeto instanceof Usuario) {
-			Usuario outro = (Usuario) objeto;
-			
-			boolean nomeIgual = outro.nome.equals(this.nome);
-			boolean emailIgual = outro.email.equals(email);
-			
-			return nomeIgual && emailIgual;	
-		} else {
-			return false;
-		}		
-		}
-	// O hashcode será abordado em outra aula!	
-	public int hashcode() {
-		return 0;
-	}
+
 }
