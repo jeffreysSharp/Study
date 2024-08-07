9b5450ff10af00bf2d567c3634e17f80b6f63bbe
Jeferson Almeida <jeferson.almeida@nava.com.br>
2024-08-07 19:06:21 -0300

    Classe Tabuleiro #02

--- FormacaoFullStack/back-end/Java/JavaCompleto/campo-minado/src/br/com/cod3r/cm/modelo/Tabuleiro.java
+++ FormacaoFullStack/back-end/Java/JavaCompleto/campo-minado/src/br/com/cod3r/cm/modelo/Tabuleiro.java
@@ -1,6 +1,7 @@
 package br.com.cod3r.cm.modelo;
 
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.ArrayList;
 
 public class Tabuleiro {
@@ -39,8 +40,28 @@ public class Tabuleiro {
 	}
 	
 	private void sortearMinas() {
+		long minasArmadas = 0;
+		Predicate<Campo> minado = c -> c.isMinado();
+		
+		
+		do {
+			minasArmadas = campos.stream().filter(minado).count();
+			int aleatorio = (int) (Math.random() * campos.size());
+			campos.get(aleatorio).minar();
+		} while(minasArmadas < minas);
 			
 	}
 	
-
+	public boolean objetivoAlcancado() {
+		return campos.stream().allMatch(c -> c.objetivoAlcancado());
+	}
+	
+	public void reiniciar() {
+		campos.stream().forEach(c -> c.reiniciar());
+		sortearMinas();
+	}
+	
+	public String toString() {
+		
+	}
 }
