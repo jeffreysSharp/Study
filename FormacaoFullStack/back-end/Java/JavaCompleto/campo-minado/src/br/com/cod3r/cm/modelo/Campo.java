15a1ec577d83844844be6111f48ffa1567427949
Jeferson Almeida <jeferson.almeida@nava.com.br>
2024-08-07 09:40:58 -0300

    Classe campo: atributos

--- FormacaoFullStack/back-end/Java/JavaCompleto/campo-minado/src/br/com/cod3r/cm/modelo/Campo.java
+++ FormacaoFullStack/back-end/Java/JavaCompleto/campo-minado/src/br/com/cod3r/cm/modelo/Campo.java
@@ -1,5 +1,15 @@
 package br.com.cod3r.cm.modelo;
 
 public class Campo {
+	
+	private final linha;
+	private final coluna;
+	private boolean minado;
+	
+	Campo(int linha, int coluna) {
+		this.coluna = coluna;
+		this.linha = linha; 
+		
+	}
 
 }
