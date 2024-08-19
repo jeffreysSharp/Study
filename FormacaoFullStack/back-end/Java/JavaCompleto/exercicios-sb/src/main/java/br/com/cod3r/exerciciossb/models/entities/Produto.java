23c8cd2ca5572ad99c35906a43c00a343ec56187
Jeferson Almeida <jeferson.almeida@nava.com.br>
2024-08-19 10:12:49 -0300

    Novos Atributos

--- FormacaoFullStack/back-end/Java/JavaCompleto/exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/models/entities/Produto.java
+++ FormacaoFullStack/back-end/Java/JavaCompleto/exercicios-sb/src/main/java/br/com/cod3r/exerciciossb/models/entities/Produto.java
@@ -7,20 +7,24 @@ import jakarta.persistence.Id;
 
 @Entity
 public class Produto {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
 	private String nome;
-	
+	private double preco;
+	private double desconto;
+
 	public Produto() {
 
 	}
 
-	public Produto(String nome) {
+	public Produto(String nome, double preco, double desconto) {
 		super();
 		this.nome = nome;
+		this.preco = preco;
+		this.desconto = desconto;
 	}
 
 	public int getId() {
@@ -37,6 +41,22 @@ public class Produto {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public double getDesconto() {
+		return desconto;
+	}
+
+	public void setDesconto(double desconto) {
+		this.desconto = desconto;
 	}
 
 }
