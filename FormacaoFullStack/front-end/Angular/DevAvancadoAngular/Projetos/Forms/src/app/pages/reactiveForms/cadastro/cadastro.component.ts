a6b91d89ca47f360ccae0a075956d51243e4fdee
Jeferson Almeida <jeferson.almeida@nava.com.br>
2024-08-24 04:57:27 -0300

    Create form

--- FormacaoFullStack/front-end/Angular/DevAvancadoAngular/Projetos/Forms/src/app/pages/reactiveForms/cadastro/cadastro.component.ts
+++ FormacaoFullStack/front-end/Angular/DevAvancadoAngular/Projetos/Forms/src/app/pages/reactiveForms/cadastro/cadastro.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Usuario } from '../../../models/usuario.model';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
-export class CadastroComponent {
+export class CadastroComponent implements OnInit {
+
+  cadastroForm: FormGroup | any;
+  usuario: Usuario | any;
+
+  constructor(private fb: FormBuilder) {
+    
+  }
+
+  ngOnInit(): void {  
+    this.cadastroForm = this.fb.group({
+      nome: [''],
+      cpf: [''],
+      email: [''],
+      senha: [''],
+      confirmeSenha: ['']
+    });
+  }
+
+  adicionarUsuario() {
+    this.usuario = Object.assign({}, this.usuario, this.cadastroForm);  
+  }
 
 }
