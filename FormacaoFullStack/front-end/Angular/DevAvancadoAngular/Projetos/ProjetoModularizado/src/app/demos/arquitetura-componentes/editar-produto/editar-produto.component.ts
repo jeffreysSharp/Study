3b07d607f708f51415ae0dcd3b7ebfad93d916c8
jeffreysSharp <jefferson_qi3@hotmail.com>
2024-08-29 11:41:37 -0300

    Navegação Imperativa com Router API

--- FormacaoFullStack/front-end/Angular/DevAvancadoAngular/Projetos/ProjetoModularizado/src/app/demos/arquitetura-componentes/editar-produto/editar-produto.component.ts
+++ FormacaoFullStack/front-end/Angular/DevAvancadoAngular/Projetos/ProjetoModularizado/src/app/demos/arquitetura-componentes/editar-produto/editar-produto.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Produto } from '../models/produto';
 import { ProdutoService } from '../services/produto.service';
 
@@ -13,7 +13,10 @@ export class EditarProdutoComponent implements OnInit {
 
   produto: Produto;
 
-  constructor(private route: ActivatedRoute, private produtoService: ProdutoService) { }
+  constructor(private route: ActivatedRoute,
+    private produtoService: ProdutoService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
 
@@ -27,6 +30,9 @@ export class EditarProdutoComponent implements OnInit {
   }
 
   salvar() {
+
+    this.router.navigate(['/produtos']);
+    // this.router.navigateByUrl('/produtos');
 
   }
 }
