4a7e5e652cb2fdbaae7a74e0e627b343054882fe
jeffreysSharp <jefferson_qi3@hotmail.com>
2024-09-05 10:00:56 -0300

    Configuração de State e Store

--- FormacaoFullStack/front-end/Angular/DevAvancadoAngular/Projetos/ProjetoModularizado/src/app/demos/todo-list/todo.module.ts
+++ FormacaoFullStack/front-end/Angular/DevAvancadoAngular/Projetos/ProjetoModularizado/src/app/demos/todo-list/todo.module.ts
@@ -1,13 +1,14 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 
-import { TasksService } from './todo.service';
 import { TasksFinalizadasComponent } from './components/tasks-finalizadas/tasks-finalizadas.component';
 import { TasksIniciadasComponent } from './components/tasks-iniciadas/tasks-iniciadas.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { ToDoListComponent } from './components/todo-list/todo-list.component';
 import { TodoComponent } from './todo.component';
+import { TasksService } from './todo.service';
+import { Store } from './todo.store';
 
 @NgModule({
   imports: [
@@ -15,7 +16,8 @@ import { TodoComponent } from './todo.component';
     HttpClientModule
   ],
   providers: [
-    TasksService
+    TasksService,
+    Store
   ],
   declarations: [
     TodoComponent,
@@ -32,4 +34,4 @@ import { TodoComponent } from './todo.component';
     ToDoListComponent
   ]
 })
-export class TodoModule {}
+export class TodoModule { }
