0c86c499d1cf47a26b1f617f209b72f75b29e8e1
Jeferson Almeida <jeferson.almeida@nava.com.br>
2024-09-21 11:13:41 -0300

    Gerando uma base de dados

---
+++ FormacaoFullStack/back-end/DotNet/07_FundamentosASPNETMVC/MinhaPrimeiraApp/MinhaPrimeiraApp/Migrations/20240921140923_v02_PrimeiraApp.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MinhaPrimeiraApp.Migrations
+{
+    public partial class v02_PrimeiraApp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Alunos",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Alunos",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Ativo",
+                table: "Alunos",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Avaliacao",
+                table: "Alunos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataNascimento",
+                table: "Alunos",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Ativo",
+                table: "Alunos");
+
+            migrationBuilder.DropColumn(
+                name: "Avaliacao",
+                table: "Alunos");
+
+            migrationBuilder.DropColumn(
+                name: "DataNascimento",
+                table: "Alunos");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Alunos",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Alunos",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60);
+        }
+    }
+}
