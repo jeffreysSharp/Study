b95c433752802396f7d48d12db30901315c91b98
Jeferson Almeida <jeferson.almeida@nava.com.br>
2024-09-21 11:32:42 -0300

    Operações no banco de dados com EF

---
+++ FormacaoFullStack/back-end/DotNet/07_FundamentosASPNETMVC/MinhaPrimeiraApp/MinhaPrimeiraApp/Controllers/TestEFController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MinhaPrimeiraApp.Data;
+using MinhaPrimeiraApp.Models;
+
+namespace MinhaPrimeiraApp.Controllers
+{
+    public class TestEFController : Controller
+    {
+        public AppDbContext Db { get; set; }
+
+        public TestEFController(AppDbContext db)
+        {
+            Db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var aluno = new Aluno()
+            {
+                Nome = "Eduardo",
+                Email = "hello@eduardo.com",
+                DataNascimento = new DateTime(1982, 04, 24),
+                Avaliacao = 5,
+                Ativo = true
+            };
+
+            //Db.Alunos.Add(aluno);
+            //Db.SaveChanges();
+
+             var alunosChange = Db.Alunos.Where(a => a.Nome.Contains("Eduardo")).FirstOrDefault();
+            //alunosChange.Nome = "Eduardo Pires";
+
+
+            //Db.Alunos.Update(alunosChange);
+            //Db.SaveChanges();
+
+            Db.Alunos.Remove(alunosChange);
+            Db.SaveChanges();
+
+            return Content("");
+        }
+    }
+}
